5958bbb3531718cca5558941c15213017ffed998
d-salazar <d.salazar.sepulveda@gmail.com>
2013-10-24 13:39:17 -0300

    version 0.21

--- js/login.js
+++ js/login.js
@@ -19,4 +19,48 @@ $('input').change(function(){
 			}
 		});
 	}
+	guardar();
 });
+
+/*
+$('#limpiar').click(function(){
+	$('form').each(function(){
+		$(this).reset();
+	});
+	localStorage.clear();
+});
+*/
+
+$(document).ready(function(){
+	if(localStorage!=null){
+		$('#aula_u').val(localStorage['aula_u']);
+		$('#aula_p').val(localStorage['aula_p']);
+
+		$('#siga_u').val(localStorage['siga_u']);
+		$('#siga_e').val(localStorage['siga_e']);
+		$('#siga_p').val(localStorage['siga_p']);
+
+		$('#moodle_inf_u').val(localStorage['moodle_inf_u']);
+		$('#moodle_inf_p').val(localStorage['moodle_inf_p']);
+
+		$('#sg_industrias_u').val(localStorage['sg_industrias_u']);
+		$('#sg_industrias_e').val(localStorage['sg_industrias_e']);
+		$('#sg_industrias_p').val(localStorage['sg_industrias_p']);
+	}
+});
+
+function guardar(){
+	localStorage['aula_u'] = $('#aula_u').val();
+	localStorage['aula_p'] = $('#aula_p').val();
+
+	localStorage['siga_u'] = $('#siga_u').val();
+	localStorage['siga_e'] = $('#siga_e').val();
+	localStorage['siga_p'] = $('#siga_p').val();
+
+	localStorage['moodle_inf_u'] = $('#moodle_inf_u').val();
+	localStorage['moodle_inf_p'] = $('#moodle_inf_p').val();
+
+	localStorage['sg_industrias_u'] = $('#sg_industrias_u').val();
+	localStorage['sg_industrias_e'] = $('#sg_industrias_e').val();
+	localStorage['sg_industrias_p'] = $('#sg_industrias_p').val();
+}
